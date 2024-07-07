4e0e1b180d92fd02310a306bf8a4045a6ebd57f5
artem8281 <mrsit8281@yandex.ru>
2024-07-07 21:42:45 +0300

    first_commit

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def connect_web_socket():
+    connection = 'socket'
+    return connection
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
